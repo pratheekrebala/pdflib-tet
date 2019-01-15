432d9128cdfa8e8bf0e31a4016c4f9c40eb97f3c
pratheekrebala <pratheekrebala@gmail.com>
2019-01-15 01:05:23 -0500

    Clean up

--- data/convert_to_html.sh
+++
@@ -1,3 +0,0 @@
-tet --verbose 2 --tetml page --docopt "$(cat ./docoptions)" --pageopt "$(cat ./pageoptions)" --targetdir $1
-tet --verbose 2 --text --docopt "$(cat ./docoptions)" --pageopt "$(cat ./pageoptions)" --targetdir $1
-#xsltproc -o $1.html tetml2html.xsl `basename $1 .pdf`.tetml
